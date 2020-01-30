cb996c526ba7ed077c271bce124efbe9dbff8b95
Raslan <24810123+aliraslan@users.noreply.github.com>
2020-01-30 03:07:18 +0200

    Added Rich Embed

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// Enable strict JavaScript mode to enforce proper code.
 'use strict';
+
+// IMPORTS
 const Discord = require('discord.js');
 const axios = require('axios');
 const ytdl = require('ytdl-core');
+
+// Initialization
 const client = new Discord.Client();
+
+/**
+ * @description Holds all the current locations where the bot is connected
+ */
 let dispatches = [];
+
+/**
+ * @description Takes a discord message and asynchronously replies to it with a random photo of sushi from Unsplash.
+ * @param {DiscordMessage} message
+ */
 const sendUnsplash = async message => {
   await message.channel.send({
     files: [
@@ -15,19 +29,40 @@ const sendUnsplash = async message => {
   });
 };
 
+/**
+ * @description Takes a discord message and asynchronously plays music from YouTube
+ * @param {DiscordMessage} message
+ */
 const playMusic = async message => {
   try {
+    // Normalize message content
     const text = message.content.toLowerCase();
+    // Get the targeted voice channel
     const { voiceChannel } = message.member;
+    // If the user isn't a voice channel
     if (!voiceChannel)
       return message.reply('Please join a voice channel first!');
+
+    // Parsing the command
     if (text.includes('play')) {
+      // Check if already playing and clear
       if (dispatches.length) {
         const dispatcher = dispatches.pop();
         dispatcher.destroy();
         dispatches = [];
       }
+      // Extract search string from command
       const searchString = text.substring(text.indexOf('play') + 5);
+      /**
+       * @description Holds the data for the YouTube API
+       * @property {baseUrl}: The base URL for the YouTube API
+       * @property {part}: The video identifier
+       * @property {type}: What to search for
+       * @property {order}: Sorting order for the results
+       * @property {maxResults}: Number of results to return
+       * @property {q}: The search string
+       * @property {key}: YouTube API Key
+       */
       const api = {
         baseUrl: 'https://www.googleapis.com/youtube/v3/search?',
         part: 'snippet',
@@ -37,24 +72,35 @@ const playMusic = async message => {
         q: searchString,
         key: process.env.KEY
       };
+      // Forming the URL from the properties.
       const apiUrl = `${api.baseUrl}part=${api.part}&type=${api.type}&maxResults=${api.maxResults}&order=${api.order}&q=${api.q}&key=${api.key}`;
-      const res = await axios.get(apiUrl);
-      const videoId = res.data.items[0].id.videoId;
+      // Querying the URL to return the video(s).
+      const response = await axios.get(apiUrl);
+      // Extract first video object
+      const video = response.data.items[0];
+      // Extract ID
+      const videoId = video.id.videoId;
+      // Join the voice channel
       voiceChannel.join().then(connection => {
+        // Create a Rich Embed to reply with.
         const playingEmbed = new Discord.RichEmbed()
-          .setColor('#aa0000')
-          .setTitle(`${res.data.items[0].snippet.title}`)
+          .setColor('#e0aca8')
+          .setTitle(`${video.snippet.title}`)
           .setURL(`https://www.youtube.com/watch?v=${videoId}`)
-          .setThumbnail(`${res.data.items[0].snippet.thumbnails.medium.url}`)
-          .setImage(`${res.data.items[0].snippet.thumbnails.medium.url}`)
+          .setThumbnail(`${video.snippet.thumbnails.medium.url}`)
           .setTimestamp();
+
+        // Send the Embed
         message.channel.send(playingEmbed);
+        // Create a youtube-dl stream to play
         const stream = ytdl(`https://www.youtube.com/watch?v=${videoId}`, {
           filter: 'audioonly'
         });
-        if (dispatches.length) dispatches.pop();
+        // Start playing the music.
         const dispatcher = connection.playStream(stream);
+        // Tracking the current dispatched stream in dispatches array.
         dispatches.push(dispatcher);
+        // When song ends, leave.
         dispatcher.on('end', () => {
           dispatcher.destroy();
           dispatches = [];
@@ -62,6 +108,7 @@ const playMusic = async message => {
         });
       });
     } else if (text.includes('stop')) {
+      // Kill dispatch
       if (dispatches.length) {
         const dispatcher = dispatches.pop();
         dispatcher.destroy();
