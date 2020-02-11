c09a4152274fa1cd789f5c9f521a608b4c5c85d6
Raslan <24810123+aliraslan@users.noreply.github.com>
2020-02-11 17:51:47 +0200

    FIXES. PENDING: YouTube.

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const playMusic = async (currentlyPlaying, connection, message) => {
       message.reply('Please join a voice channel first.');
     }
     if (text.includes('play')) {
-      if (currentlyPlaying) {
+      if (currentlyPlaying.snippet.title) {
         const searchString =
           text === 'sushi play'
             ? 'late night piano'
